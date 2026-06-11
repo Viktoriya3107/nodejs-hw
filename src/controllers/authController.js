a16431e7260c580b32586e9646e015c3adfdd586
Viktoriya3107 <bondarenko_vikyoriya988@ukr.net>
2026-06-11 22:00:14 +0300

    fix: replace bcrypt with bcryptjs

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
